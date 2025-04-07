b307444aa816b8e8db4b76e9525d6b3bca6beb7e
sudeep-h <sudeephipparge@gmail.com>
2025-04-07 16:59:27 +0530

    Event Listeners

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,9 @@ import ExpenseDate from "./ExpenseData";
 import Card from "./Card.js";
 
 function ExpenseItem(props){
-    
+    function changeTitleHandler(){
+        console.log("Button clicked");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />  
@@ -12,6 +14,7 @@ function ExpenseItem(props){
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
             </div>
+            <button onClick={changeTitleHandler}>Change Title</button>
         </Card>
     )
 }
